ff0164d9172651fa4e01c4432090e2071bb47ceb
akashchavan4004 <160306065+akashchavan4004@users.noreply.github.com>
2024-03-04 14:57:05 +0530

    add swicth button

--- admin/customer.php
+++ admin/customer.php
@@ -28,37 +28,59 @@
 						</thead>
 						<tbody>
 							<?php
-							$i=0;
+							$i = 0;
 							$statement = $pdo->prepare("SELECT * 
 														FROM tbl_customer t1
 														JOIN tbl_country t2
 														ON t1.cust_country = t2.country_id
 													");
 							$statement->execute();
-							$result = $statement->fetchAll(PDO::FETCH_ASSOC);					
+							$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 							foreach ($result as $row) {
 								$i++;
 								?>
-								<tr class="<?php if($row['cust_status']==1) {echo 'bg-g';}else {echo 'bg-r';} ?>">
-									<td><?php echo $i; ?></td>
-									<td><?php echo $row['cust_name']; ?></td>
-									<td><?php echo $row['cust_email']; ?></td>
+								<tr class="<?php if ($row['cust_status'] == 1) {
+									echo 'bg-g';
+								} else {
+									echo 'bg-r';
+								} ?>">
+									<td>
+										<?php echo $i; ?>
+									</td>
+									<td>
+										<?php echo $row['cust_name']; ?>
+									</td>
+									<td>
+										<?php echo $row['cust_email']; ?>
+									</td>
 									<td>
 										<?php echo $row['country_name']; ?><br>
 										<?php echo $row['cust_city']; ?><br>
 										<?php echo $row['cust_state']; ?>
 									</td>
-									<td><?php if($row['cust_status']==1) {echo 'Active';} else {echo 'Inactive';} ?></td>
 									<td>
-										<a href="customer-change-status.php?id=<?php echo $row['cust_id']; ?>" class="btn btn-success btn-xs">Change Status</a>
+										<?php if ($row['cust_status'] == 1) {
+											echo 'Active';
+										} else {
+											echo 'Inactive';
+										} ?>
 									</td>
 									<td>
-										<a href="#" class="btn btn-danger btn-xs" data-href="customer-delete.php?id=<?php echo $row['cust_id']; ?>" data-toggle="modal" data-target="#confirm-delete">Delete</a>
+										<!-- <a href="customer-change-status.php?id=<?php echo $row['cust_id']; ?>" class="btn btn-success btn-xs">Change Status</a> -->
+										<label class="switch">
+											<input type="checkbox" 1="">
+											<span class="slider round"></span>
+										</label>
+									</td>
+									<td>
+										<a href="#" class="btn btn-danger btn-xs"
+											data-href="customer-delete.php?id=<?php echo $row['cust_id']; ?>"
+											data-toggle="modal" data-target="#confirm-delete">Delete</a>
 									</td>
 								</tr>
 								<?php
 							}
-							?>							
+							?>
 						</tbody>
 					</table>
 				</div>
@@ -70,22 +92,23 @@
 </section>
 
 
-<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Delete Confirmation</h4>
-            </div>
-            <div class="modal-body">
-                <p>Are you sure want to delete this item?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-danger btn-ok">Delete</a>
-            </div>
-        </div>
-    </div>
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+	aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="myModalLabel">Delete Confirmation</h4>
+			</div>
+			<div class="modal-body">
+				<p>Are you sure want to delete this item?</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<a class="btn btn-danger btn-ok">Delete</a>
+			</div>
+		</div>
+	</div>
 </div>
 
 
